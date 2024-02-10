671fd2c8a2171e8fb212eafdc1ed4e7eae4ece49
Fatih Toprakkale <91729123+fto-exerp@users.noreply.github.com>
2024-02-10 06:36:45 +0100

    asd

--- src/components/NoteWidget.vue
+++ src/components/NoteWidget.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-[#fff4a2] text-black w-[300px] mt-5 mr-5 rounded-[16px] p-4 drop-shadow-md">
         <h1 class="text-2xl font-bold">Notes</h1>
-        <p><span class="font-bold">Hello Visitor!</span><br> this website is still under development. if you want to see my projects along with this project you can find it all on my <a class="underline" href="https://github.com/fatihtkale">Github</a></p>
+        <p><span class="font-bold">Hello Visitor!</span><br> this website is still under development. if you want to see my projects along with this project you can find it all on my <a class="underline" href="https://github.com/fatihtkale">Github.</a></p>
     </div>
 </template>
