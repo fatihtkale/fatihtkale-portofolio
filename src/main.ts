49e67f640046d7501ce30e1772df3b5936483efb
Fatih Toprakkale <91729123+fto-exerp@users.noreply.github.com>
2024-02-10 06:33:34 +0100

    asd

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import VueDraggableResizable from 'vue-draggable-resizable'
+// @ts-ignore
+import * as VueDraggableResizable from 'vue-draggable-resizable'
 
 createApp(App)
     .component("vue-draggable-resizable", VueDraggableResizable)
